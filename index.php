5a6348b541b09a9ea04f2aee5bcf44fc3c731f19
Nur Achmad Shafy Raissa Tsani <75611252+achmadxds@users.noreply.github.com>
2021-02-08 12:01:22 +0700

    new

--- index.php
+++ index.php
@@ -1,58 +1,5 @@
 <?php
-session_start();
-$connect = new mysqli('localhost', 'root', '', 'calendar');
-
-if (mysqli_connect_errno()) {
-	echo "koneksi database gagal : " . mysqli_connect_error();
-	die();
-}
-
-function query($query){
-	global $connect;
-	$result   = mysqli_query($connect, $query);
-	$rows     = [];
-	while ($row = mysqli_fetch_assoc($result)) {
-		$rows[] = $row;
-	}
-	return $rows;
-
-}
-
-function week_max($year){
-	$date = new DateTime;
-	$date->setISODate($year, 53);
-	return ($date->format('W') == '53' ? 53 : 52);
-}
-
-function week_number($date){
-	$week      = date('W', strtotime($date));
-	$day       = date('N', strtotime($date));
-	$max_weeks = week_max(date('Y', strtotime($date)));
-
-	if ($day == 7 && $week < $max_weeks) {
-		return ++$week;
-	} else if ($day == 7) {
-		return 1;
-	} else {
-		return $week;
-	}
-}
-
-$period = new DatePeriod(
-	new DateTime('2021-01-01'),
-	new DateInterval('P1D'),
-	new DateTime('2022-01-01')
-);
-
-$days 				 = [];
-foreach ($period as $v) {
-	$week_number = week_number($v->format('Y-m-d'));
-	$week_number = str_pad($week_number, 2, '0', STR_PAD_LEFT);
-	$days[$v->format('n')][$week_number][$v->format('w')] = $v->format('Y-m-d');
-}
-
-$days_name   		   = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-$months_name 		   = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+include 'auth.php';
 $users      		   = query('SELECT `id`, `name` FROM `user` WHERE 1');
 
 if($_SESSION['admin']){
@@ -85,27 +32,6 @@ if(isset($_POST['login'])){
 	} else {
 		echo "<script> alert('salah username'); </script>";
 	}
-}
-
-	// HOLIDAY
-function GetFirstNationHoliday()
-{
-	$holiday_value = file_get_contents('https://kalenderindonesia.com/api/APIHPnFCegVK2/libur/masehi/2021');
-	$holiday_value = json_decode($holiday_value, 1);
-
-	$holidays      = [];
-
-	foreach ($holiday_value as $value) {
-		foreach ($value['holiday'] as $value1) {
-			$data1 = $value1['data'];
-			foreach ($data1 as $hldys) {
-				if(array_key_exists("holiday", $holiday_value['data']))
-					$holidays[$hldys['date']] = $hldys['name'];
-			}
-		}
-	}
-
-	return $holidays;
 }
 
 	// HOLIDAY ESP
@@ -333,9 +259,6 @@ if(isset($_POST['task'])){
 					</div>
 				</div>
 			</div>
-
-
-
 		</body>
 		<!-- jQuery -->
 		<script src="//code.jquery.com/jquery.js"></script>
--- users.php
+++ users.php
@@ -1,11 +1,5 @@
 <?php
-session_start();
-$connect = new mysqli('localhost', 'root', '', 'calendar');
-
-if (mysqli_connect_errno()) {
-	echo "koneksi database gagal : " . mysqli_connect_error();
-	die();
-}
+include 'auth.php';
 
 if(!$_SESSION['karyawan']){
 	header('LOCATION: index.php');
@@ -18,75 +12,6 @@ if(isset($_POST['logout'])){
 	$_SESSION['nama'] = "";
 	header('LOCATION: index.php');
 	exit;
-}
-
-function query($query){
-	global $connect;
-	$result   = mysqli_query($connect, $query);
-	$rows     = [];
-	while ($row = mysqli_fetch_assoc($result)) {
-		$rows[] = $row;
-	}
-	return $rows;
-
-}
-
-function week_max($year){
-	$date = new DateTime;
-	$date->setISODate($year, 53);
-	return ($date->format('W') == '53' ? 53 : 52);
-}
-
-function week_number($date){
-	$week      = date('W', strtotime($date));
-	$day       = date('N', strtotime($date));
-	$max_weeks = week_max(date('Y', strtotime($date)));
-
-	if ($day == 7 && $week < $max_weeks) {
-		return ++$week;
-	} else if ($day == 7) {
-		return 1;
-	} else {
-		return $week;
-	}
-}
-
-$period = new DatePeriod(
-	new DateTime('2021-01-01'),
-	new DateInterval('P1D'),
-	new DateTime('2022-01-01')
-);
-
-$days 				 = [];
-foreach ($period as $v) {
-	$week_number = week_number($v->format('Y-m-d'));
-	$week_number = str_pad($week_number, 2, '0', STR_PAD_LEFT);
-	$days[$v->format('n')][$week_number][$v->format('w')] = $v->format('Y-m-d');
-}
-
-$days_name   		   = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-$months_name 		   = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-$users      		   = query('SELECT `id`, `name` FROM `user` WHERE `name`="'.$_SESSION['nama'].'" ');
-
-	// HOLIDAY
-function GetFirstNationHoliday()
-{
-	$holiday_value = file_get_contents('https://kalenderindonesia.com/api/APIHPnFCegVK2/libur/masehi/2021');
-	$holiday_value = json_decode($holiday_value, 1);
-
-	$holidays      = [];
-
-	foreach ($holiday_value as $value) {
-		foreach ($value['holiday'] as $value1) {
-			$data1 = $value1['data'];
-			foreach ($data1 as $hldys) {
-				if(array_key_exists("holiday", $holiday_value['data']))
-					$holidays[$hldys['date']] = $hldys['name'];
-			}
-		}
-	}
-
-	return $holidays;
 }
 
 	// HOLIDAY ESP
@@ -140,7 +65,7 @@ function GetDateNotes(){
 		`nt`.`date_holiday`,
 		`nt`.`id`
 		FROM `notes` as `nt` LEFT JOIN `user` as `us` 
-		ON `nt`.`user_id` = `us`.`id` WHERE `nt`.`date_holiday` = "'.$dates1.'" AND `us`.`nama`="'.$_SESSION['nama'].'" ');
+		ON `nt`.`user_id` = `us`.`id` WHERE `nt`.`date_holiday` = "'.$dates1.'" AND `us`.`name`="'.$_SESSION['nama'].'" ');
 
 	return $getdatepost;
 }
